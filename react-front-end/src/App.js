c016f92d2a56049a6beaa609694ae312ba8f258c
Rinaldo Wouterson <rinaldo@Rinaldos-MacBook-Air.local>
2022-11-24 13:11:08 +0100

    switching

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -2,25 +2,41 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import axios from 'axios'
+import jwtDecode from 'jwt-decode';
 import { useEffect, useState } from 'react';
 import ProblemList from './components/ProblemList'
 
 function App() {
+  const [user, setUser] = useState({})
+
+  const handleCallbackResponseGoogle = (response) => {
+    var user = jwtDecode(response.credential);
+    console.log({user})
+    return setUser(user);
+  }
+
 
   // const [weather, setWeather] = useState(null);
 
-  // useEffect(() => {
-  //  
-  //   const test = async () => {
-  //     const test = await axios.get("https://opobackend.azurewebsites.net/WeatherForecast").then((res)=>console.log(res.data))
-  //   }
-  //   test();
-  // })
+  useEffect(() => {
+   /* global google */
+    google.accounts.id.initialize({
+      client_id: "1061925551073-28j75e6a29ukrfosq7otehkbe05auqj3.apps.googleusercontent.com",
+      callback: handleCallbackResponseGoogle
+    })
+
+    google.accounts.id.renderButton(
+      document.getElementById("signInDiv"),
+      { theme: "outline", size: "large" }
+    );
+  },[])
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <div id="signInDiv"></div>
+        
         <ProblemList/>
         <p>
           Edit
