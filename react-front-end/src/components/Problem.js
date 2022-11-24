20abd87c89eb66d84b3f0ecd3b9152ead0c07cf5
salt-dev <f@>
2022-11-24 14:55:16 +0100

    “switching”

--- react-front-end/src/components/Problem.js
+++ react-front-end/src/components/Problem.js
@@ -7,30 +7,30 @@ import axios from "axios";
 
 const PROBLEMS_ENDPOINT = 'https://opobackend.azurewebsites.net/api/Problems';
 
-const Problem = ( {problem, getProblems}) => {
+const Problem = ({problem, getProblems}) => {
   const {problemId} = problem
-  
+
 
   const clickDelete = async () => {
-    const {status } = await axios.delete(PROBLEMS_ENDPOINT + `/${problemId}`)
-    if(status === 204) {
+    const {status} = await axios.delete(PROBLEMS_ENDPOINT + `/${problemId}`)
+    if (status === 204) {
       getProblems();
     }
   }
-  
+
   return (
     <div>
-      <div className='problem-header' style={{display: "flex" }}>
-      <h4>{problem.title}</h4>
-      <IconButton onClick={clickDelete}>
-        <DeleteIcon/>
-      </IconButton>
+      <div className='problem-header' style={{display: "flex"}}>
+        <h4>{problem.title}</h4>
+        <IconButton onClick={clickDelete}>
+          <DeleteIcon/>
+        </IconButton>
       </div>
-      <div className='problem-lists' >
-      <ProsList className='list-c' problem={problem}/>
-      <ConsList className='list-c' problem={problem}/>
+      <div className='problem-lists'>
+        <ProsList className='list-c' problem={problem}/>
+        <ConsList className='list-c' problem={problem}/>
       </div>
-      
+
     </div>
   );
 };
--- react-front-end/src/components/ProblemList.js
+++ react-front-end/src/components/ProblemList.js
@@ -1,41 +1,40 @@
 import React, {useEffect, useState} from 'react';
 import Problem from "./Problem";
 import axios, {Axios} from "axios";
-import ProForm from "./ProForm";
 import ProblemFrom from "./ProblemFrom";
 
 const PROBLEMS_ENDPOINT = 'https://opobackend.azurewebsites.net/api/Problems';
 
 const ProblemList = () => {
   const [problems, setProblems] = useState([])
-  
+
   const getProblems = async () => {
     const {data, status} = await axios.get(PROBLEMS_ENDPOINT);
-    if(status === 200) {
+    if (status === 200) {
       setProblems(data)
     }
   }
 
   const addProblem = async (problem) => {
-    // adds new con to beginning of cons array
-    const {status } = axios.post(PROBLEMS_ENDPOINT + '?userId=1', problem)
-    if(status === 200) {
+    // adds new con to beginning of problems array
+    const {status} = await axios.post(PROBLEMS_ENDPOINT + '?userId=1', problem)
+    if (status === 200) {
       getProblems()
     }
     setProblems([problem, ...problems]);
   }
-  
+
   useEffect(() => {
-    
-     getProblems();
+
+    getProblems();
   }, [])
-  
+
   //fetch and Map problems
   return (
     <div>
       <ProblemFrom addProblem={addProblem}/>
       <ul id='Problem-List'>
-        {problems.map(problem =>  <li key={problem.id}><Problem getProblems={getProblems} problem={problem}/></li>)}
+        {problems.map(problem => <li key={problem.id}><Problem getProblems={getProblems} problem={problem}/></li>)}
       </ul>
     </div>
   );
