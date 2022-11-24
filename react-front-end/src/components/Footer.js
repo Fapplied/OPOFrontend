e79e107c19681c5be9ab17547545fd32e30f33f2
Rinaldo Wouterson <rinaldo@Rinaldos-MacBook-Air.local>
2022-11-24 16:29:29 +0100

    switching

--- react-front-end/src/components/Footer.js
+++ react-front-end/src/components/Footer.js
@@ -1,11 +1,11 @@
-import gitLogo from './GitHub_Logo.png';
+// import gitLogo from './GitHub_Logo.png';
 
 
 
 const Footer = () => {
     return <>
         <hr />
-        <a href='https://github.com/saltams'>Log in @ <img src={gitLogo} alt='Git logo' width='50px'/></a>
+        {/* <a href='https://github.com/saltams'>Log in @ <img src={gitLogo} alt='Git logo' width='50px'/></a> */}
     </>
 }
 
