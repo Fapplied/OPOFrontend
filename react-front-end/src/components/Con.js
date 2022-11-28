89ad1840e9facab9e32e795b65ab70baa72df38f
Rinaldo Wouterson <rinaldo@Rinaldos-MacBook-Air.local>
2022-11-28 13:12:48 +0100

    switching

--- react-front-end/src/components/Con.js
+++ react-front-end/src/components/Con.js
@@ -1,55 +1,67 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from "react";
 import DeleteIcon from "@mui/icons-material/Delete";
-import {Avatar, IconButton} from "@mui/material";
+import { Avatar, IconButton } from "@mui/material";
 import axios from "axios";
 import KeyboardDoubleArrowUpOutlinedIcon from "@mui/icons-material/KeyboardDoubleArrowUpOutlined";
 import KeyboardArrowUpOutlinedIcon from "@mui/icons-material/KeyboardArrowUpOutlined";
 
-const Likes_ENDPOINT = 'https://opobackend.azurewebsites.net/api/Likes/con';
-
+const Likes_ENDPOINT = "https://opobackend.azurewebsites.net/api/Likes/con";
 
 const Con = ({ con }) => {
-  const {title, conId, problemId} = con
-  const [likes, setLikes] = useState(0)
-  const [isLiked, setIsLiked] = useState(false)
-
+  const { title, conId, problemId } = con;
+  const [likes, setLikes] = useState(0);
+  const [isLiked, setIsLiked] = useState(false);
 
   const handleUpVoteClick = async () => {
-    const { data, status} = await axios.post(Likes_ENDPOINT + `?conId=${conId}`, 1);
+    const { data, status } = await axios.post(
+      Likes_ENDPOINT + `?conId=${conId}`,
+      1
+    );
     if (status === 201 || status === 204) {
-      getLikes()
-      setIsLiked(prevState => !prevState)
+      getLikes();
+      setIsLiked((prevState) => !prevState);
     }
-  }
+  };
 
   const getLikes = async () => {
-    const {data, status} = await axios.get(Likes_ENDPOINT + `/${conId}`);
-    if(status === 200 && Number.isInteger(data.length)) {
-      setLikes(data.length)
+    const { data, status } = await axios.get(Likes_ENDPOINT + `/${conId}`);
+    if (status === 200 && Number.isInteger(data.length)) {
+      setLikes(data.length);
     }
-  }
+  };
 
   useEffect(() => {
-
-    getLikes()
-  }, [])
+    getLikes();
+  }, []);
 
   return (
-
-    <div style={{display: 'flex'}}>
-      <Avatar style={{border: 'solid grey' , margin: '1vw' }} src={`https://avatars.dicebear.com/api/open-peeps/${conId}.svg`}/>
-      {location.pathname === "/user" ? <br /> :
-      <div>
-        <IconButton onClick={handleUpVoteClick} >
-          {isLiked ? <KeyboardDoubleArrowUpOutlinedIcon/> :<KeyboardArrowUpOutlinedIcon/> }
-        </IconButton>
-        <p style={{color: "black"}}>{likes}</p>
-      </div>}
+    <div style={{ display: "flex" }}>
+      <Avatar
+        style={{ border: "solid grey", margin: "1vw" }}
+        src={`https://avatars.dicebear.com/api/open-peeps/${conId}.svg`}
+      />
+      {location.pathname === "/" ? (
+        <br />
+      ) : (
+        <div>
+          <IconButton onClick={handleUpVoteClick}>
+            {isLiked ? (
+              <KeyboardDoubleArrowUpOutlinedIcon />
+            ) : (
+              <KeyboardArrowUpOutlinedIcon />
+            )}
+          </IconButton>
+          <p style={{ color: "black" }}>{likes}</p>
+        </div>
+      )}
       <p className="opinions">{title}</p>
-      {location.pathname === "/user" ?
-        <IconButton  >
+      {location.pathname === "/user" ? (
+        <IconButton>
           <DeleteIcon />
-        </IconButton> : <br /> }
+        </IconButton>
+      ) : (
+        <br />
+      )}
     </div>
   );
 };
--- react-front-end/src/components/Pro.js
+++ react-front-end/src/components/Pro.js
@@ -1,58 +1,72 @@
-import React, {useEffect, useState} from 'react';
-import {Avatar, IconButton} from "@mui/material";
-import DeleteIcon from '@mui/icons-material/Delete';
+import React, { useEffect, useState } from "react";
+import { Avatar, IconButton } from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
 import { Route, useLocation, Link } from "react-router-dom";
-import { ArrowUpwardOutlined} from "@mui/icons-material";
+import { ArrowUpwardOutlined } from "@mui/icons-material";
 import axios from "axios";
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
-import KeyboardArrowUpOutlinedIcon from '@mui/icons-material/KeyboardArrowUpOutlined';
-import KeyboardDoubleArrowUpOutlinedIcon from '@mui/icons-material/KeyboardDoubleArrowUpOutlined';
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import KeyboardArrowUpOutlinedIcon from "@mui/icons-material/KeyboardArrowUpOutlined";
+import KeyboardDoubleArrowUpOutlinedIcon from "@mui/icons-material/KeyboardDoubleArrowUpOutlined";
 
-const Likes_ENDPOINT = 'https://opobackend.azurewebsites.net/api/Likes/pro';
+const Likes_ENDPOINT = "https://opobackend.azurewebsites.net/api/Likes/pro";
 
+const Pro = ({ pro }) => {
+  const { title, proId } = pro;
+  const [likes, setLikes] = useState(0);
+  const [isLiked, setIsLiked] = useState(false);
 
-const Pro = ({pro}) => {
-  const {title, proId} = pro
-  const [likes, setLikes] = useState(0)
-  const [isLiked, setIsLiked] = useState(false)
-
-  const location = useLocation()
+  const location = useLocation();
 
   const handleUpVoteClick = async () => {
-    const {data, status} = await axios.post(Likes_ENDPOINT + `?proId=${proId}`, 2);
+    const { data, status } = await axios.post(
+      Likes_ENDPOINT + `?proId=${proId}`,
+      2
+    );
     if (status === 201 || status === 204) {
-      setIsLiked(prevState => !prevState)
-      getLikes()
+      setIsLiked((prevState) => !prevState);
+      getLikes();
     }
-  }
+  };
   const getLikes = async () => {
-    const {data, status} = await axios.get(Likes_ENDPOINT + `/${proId}`);
-    if(status === 200 && Number.isInteger(data.length)) {
-      setLikes(data.length)
+    const { data, status } = await axios.get(Likes_ENDPOINT + `/${proId}`);
+    if (status === 200 && Number.isInteger(data.length)) {
+      setLikes(data.length);
     }
-  }
-  
-  useEffect(() => {
+  };
 
-    getLikes()
-  }, [])
+  useEffect(() => {
+    getLikes();
+  }, []);
 
   return (
-    <div style={{display: 'flex'}}>
-      <Avatar style={{border: 'solid grey' , margin: '1vw' }} src={`https://avatars.dicebear.com/api/open-peeps/${proId}.svg`}/>
-      {location.pathname === "/user" ? <br /> :
-      <div>
-        <IconButton onClick={handleUpVoteClick} >
-          {isLiked ? <KeyboardDoubleArrowUpOutlinedIcon/> :<KeyboardArrowUpOutlinedIcon/> }
-        </IconButton>
-        <p style={{color: "black"}}>{likes}</p>
-      </div>}
+    <div style={{ display: "flex" }}>
+      <Avatar
+        style={{ border: "solid grey", margin: "1vw" }}
+        src={`https://avatars.dicebear.com/api/open-peeps/${proId}.svg`}
+      />
+      {location.pathname === "/" ? (
+        <br />
+      ) : (
+        <div>
+          <IconButton onClick={handleUpVoteClick}>
+            {isLiked ? (
+              <KeyboardDoubleArrowUpOutlinedIcon />
+            ) : (
+              <KeyboardArrowUpOutlinedIcon />
+            )}
+          </IconButton>
+          <p style={{ color: "black" }}>{likes}</p>
+        </div>
+      )}
       <p className="opinions">{title}</p>
-      {location.pathname === "/user" ? 
-      <IconButton  >
-        <DeleteIcon />
-      </IconButton> : <br /> }
+      {location.pathname === "/user" ? (
+        <IconButton>
+          <DeleteIcon />
+        </IconButton>
+      ) : (
+        <br />
+      )}
     </div>
   );
 };
