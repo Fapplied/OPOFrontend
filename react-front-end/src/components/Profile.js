4cee8fad0079a359847bdc4dfbeab9a1d6aed01f
Rinaldo Wouterson <rinaldo@Rinaldos-MacBook-Air.local>
2022-11-29 15:01:52 +0100

    switching

--- react-front-end/src/components/Profile.js
+++ react-front-end/src/components/Profile.js
@@ -1,15 +1,93 @@
 import React, { useEffect, useState } from "react";
-import AddAPhotoIcon from '@mui/icons-material/AddAPhoto';
+import AddAPhotoIcon from "@mui/icons-material/AddAPhoto";
+import axios from "axios";
+import { getLS, setLS } from "../helpers/storage";
+import { Avatar, IconButton } from "@mui/material";
+import { border, textAlign } from "@mui/system";
 
-const Profile = ({user}) => {
+const Profile = ({ user }) => {
+  const [picURL, setPicURL] = useState();
+
+  const handleImage = (e) => {
+    SendToAPI(e.target.files[0]);
+  };
+
+  const SendToAPI = (im) => {
+    const formData = new FormData();
+    formData.append("formFile", im);
+    const config = {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    };
+
+    axios
+      .post(
+        `https://opobackend.azurewebsites.net/api/ProfilePictures/${
+          user.userId ?? user.data.userId
+        }`,
+        formData,
+        config
+      )
+      .then((apiResponse) => {
+        console.log({ apiResponse });
+
+        setPicURL(apiResponse.data.url);
+        return apiResponse;
+      })
+      .catch((err) => {
+        console.log({ axios: err });
+      });
+  };
+
+  useEffect(() => {
+    const trial = getLS("profpic");
+    console.log({ trial });
+    if (trial === false) {
+      axios
+        .get(
+          `https://opobackend.azurewebsites.net/api/ProfilePictures/${
+            user.userId ?? user.data.userId
+          }`
+        )
+        .then((res) => {
+          setLS("profpic", res.data);
+          setPicURL(res.data.url);
+        });
+    } else {
+      setPicURL(trial);
+    }
+  }, []);
 
   return (
     <div>
-     <img src="" alt="Profile Picture"/>
-      <AddAPhotoIcon/>
+      <Avatar
+        style={{
+          width: "100px",
+          height: "100px",
+          border: "solid grey",
+          textAlign: "center",
+        }}
+        src={picURL ?? ""}
+        alt="Profile Picture"
+      />
+      <input
+        onChange={handleImage}
+        className="hidden"
+        type="file"
+        id="my_file"
+      ></input>
+      <IconButton
+        onClick={() => {
+          document.getElementById("my_file").click();
+        }}
+      >
+        <AddAPhotoIcon />
+      </IconButton>
+
       <h3>{user.name}</h3>
     </div>
-);
+  );
 };
 
 export default Profile;
